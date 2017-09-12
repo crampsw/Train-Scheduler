92574190b50d0e7774d98ed8d93bfa2eaf2c8af7
unknown <crampsw@gmail.com>
2017-09-12 18:06:00 -0400

    added firebase init

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,12 @@
-var giffer = {
-    topics: [],
-    
-}
+ 
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAW4M88EVOuogxhh-2MW3NVkqhRGb1RQW0",
+    authDomain: "train-scheduler-60858.firebaseapp.com",
+    databaseURL: "https://train-scheduler-60858.firebaseio.com",
+    projectId: "train-scheduler-60858",
+    storageBucket: "train-scheduler-60858.appspot.com",
+    messagingSenderId: "712278560678"
+  };
+  firebase.initializeApp(config);
